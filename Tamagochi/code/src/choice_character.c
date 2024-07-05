8d715bdd3fd8e8ee635adff25535bba80e9b26e5
Giga-Sui <suigiga732@gmail.com>
2024-07-05 19:21:21 +0200

    Fix the parameters choices

--- Tamagochi/code/src/choice_character.c
+++ Tamagochi/code/src/choice_character.c
@@ -3,10 +3,18 @@
 
 #include "choice_anime.h"
 
-int choice_character(void)
+int choice_character(int choose_a)
 {
-    if ( == 1)
+    if (choose_a == 1)
     {
-
+        // Define the choice's variable
+        int choose_c;
+        // Characters list
+        printf("1. Naruto\n");
+        printf("2. Hinata\n");
+        printf("3. Neji\n");
+        printf("Please choose your anime : ");
+        // Input for choice
+        scanf("%d", &choose_c);
     }
 }
