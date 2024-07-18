88e220b91a8b583dde80a14188713f7d68a15a7b
Giga-Sui <suigiga732@gmail.com>
2024-07-18 17:35:42 +0200

    Add some decorations

--- Python_tamagochi/Graphics/app_animes.py
+++ Python_tamagochi/Graphics/app_animes.py
@@ -11,19 +11,20 @@ class App_animes:
         self.root.title("Otakushi")
         self.root.maxsize(240, 320)
         self.root.minsize(240, 320)
+        self.root.config(bg='white')
 
         # Widgets
-        self.label = Label(self.root, text="Please choose your anime").pack()
+        self.label = Label(self.root, text="Please choose your anime", bg='white', font=('Arrial', 10, 'bold')).pack()
 
         self.center_frame = Frame(self.root).pack()
 
-        self.naruto_label = Label(self.center_frame, text="1.").pack()
-        self.naruto_button = Button(self.root, text="Naruto", command=Naruto).pack()
+        self.naruto_label = Label(self.center_frame, text="1.", bg='white', font=('Arrial', 10, 'bold')).pack()
+        self.naruto_button = Button(self.root, text="Naruto", command=Naruto, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack()
 
-        self.demon_slayer_label = Label(self.center_frame, text="2.").pack()
-        self.demon_slayer_button = Button(self.root, text="Demon Slayer", command=Demon_slayer).pack()
+        self.demon_slayer_label = Label(self.center_frame, text="2.", bg='white', font=('Arrial', 10, 'bold')).pack()
+        self.demon_slayer_button = Button(self.root, text="Demon Slayer", command=Demon_slayer, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack()
 
-        self.one_punch_man_label = Label(self.center_frame, text="3.").pack()
-        self.one_punch_man_button = Button(self.root, text="One Punch Man", command=One_punch_man).pack()
+        self.one_punch_man_label = Label(self.center_frame, text="3.", bg='white', font=('Arrial', 10, 'bold')).pack()
+        self.one_punch_man_button = Button(self.root, text="One Punch Man", command=One_punch_man, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack()
 
         self.root.mainloop()
--- Python_tamagochi/Graphics/reactions.py
+++ Python_tamagochi/Graphics/reactions.py
@@ -18,27 +18,28 @@ class Reactions:
         self.reactions.title("Otakushi")
         self.reactions.maxsize(240, 320)
         self.reactions.minsize(240, 320)
+        self.reactions.config(bg='white')
 
         self.reacts = random.randint(1, 4)
 
         if self.reacts == 1:
-            self.k1 = Label(self.reactions, text="Is hungry").pack()
+            self.k1 = Label(self.reactions, text="Is hungry", bg='white', font=('Arrial', 10, 'bold')).pack()
             self.wtd = Entry(self.reactions).pack(fill=X)
-            self.wtd_button = Button(self.reactions, text="Validate", command=valid).pack(fill=X)
+            self.wtd_button = Button(self.reactions, text="Validate", command=valid, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack(fill=X)
 
         if self.reacts == 2:
-           self.k2 = Label(self.reactions, text="Is dirty").pack()
+           self.k2 = Label(self.reactions, text="Is dirty", bg='white', font=('Arrial', 10, 'bold')).pack()
            self.wtd2 = Entry(self.reactions).pack(fill=X)
-           self.wtd2_button = Button(self.reactions, text="Validate", command=valid).pack(fill=X)
+           self.wtd2_button = Button(self.reactions, text="Validate", command=valid, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack(fill=X)
                 
         if self.reacts == 3:
-            self.k3 = Label(self.reactions, text="Is sick").pack()
+            self.k3 = Label(self.reactions, text="Is sick", bg='white', font=('Arrial', 10, 'bold')).pack()
             self.wtd3 = Entry(self.reactions).pack(fill=X)
-            self.wtd3_button = Button(self.reactions, text="Validate", command=valid).pack(fill=X)
+            self.wtd3_button = Button(self.reactions, text="Validate", command=valid, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack(fill=X)
 
         if self.reacts == 4:
-            self.k4 = Label(self.reactions, text="Wants to play").pack()
+            self.k4 = Label(self.reactions, text="Wants to play", bg='white', font=('Arrial', 10, 'bold')).pack()
             self.wtd4 = Entry(self.reactions).pack(fill=X)
-            self.wtd4_button = Button(self.reactions, text="Validate", command=valid).pack(fill=X)
+            self.wtd4_button = Button(self.reactions, text="Validate", command=valid, bg='black', fg='white', activebackground='white', activeforeground='black', font=('Arrial', 10, 'bold')).pack(fill=X)
 
         self.reactions.mainloop()
